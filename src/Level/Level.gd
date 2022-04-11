edf5ea7e679e7301f9544506fe2777b1f1077f70
vanja-g <101061983+vanja-g@users.noreply.github.com>
2022-04-10 18:44:37 -0600

    Added Level scene with random level generation
    To add:
    *Some walls missing collision shapes
    *Rooms vary based on number of exits
    *Add player to level
    *Add interactions between player and level(enter/leave rooms)
    *Camera on room

--- src/Level/Level.gd
+++ src/Level/Level.gd
@@ -1,2 +1,87 @@
 extends Node2D
 
+const ROOMS:Dictionary={
+	0:preload("res://src/Rooms/Room0.tscn"),
+	1:preload("res://src/Rooms/Room1.1.tscn"),
+	2:preload("res://src/Rooms/Room1.2.tscn"),
+	3:preload("res://src/Rooms/Room1.3.tscn")
+	}
+export (int) var num_rooms =  6
+var level:Array = []
+const REVERSE_DIRECTION = {"bot":"top","top":"bot","right":"left","left":"right"}
+var current_room = null
+
+func make_level() ->void:
+	var rng = RandomNumberGenerator.new()
+	var remaining_rooms = num_rooms
+	var start_room = ROOMS[0].instance()
+	
+	level.append(start_room)
+	var to_connect:Array= [start_room]
+	add_child(start_room,true)
+	
+	current_room = start_room
+	
+	while remaining_rooms >0:
+		
+		var tmp_level = []
+		for item in level:
+			tmp_level.append(item)
+		for room in tmp_level:
+			if room in to_connect and remaining_rooms >0:
+				var directions = shuffleList(room.available_directions())
+				randomize()
+				for i in range (rng.randi_range(1,len(directions))):
+					if remaining_rooms<=0:
+						break
+					randomize()
+					var pick_room = rng.randi_range(0,len(ROOMS)-1)
+					var next_room = ROOMS[pick_room].instance()
+					level.append(next_room)
+					add_child(next_room,true)
+					next_room.visible = false
+					connect_rooms(room,next_room,directions[i-1])
+					to_connect.append(next_room)
+					remaining_rooms -=1
+				to_connect.erase(room)
+
+func connect_rooms(room1,room2,direction):
+
+		assert (room1 in level)
+		assert (room2 in level)
+		
+		room1.add_connection(room2,direction)
+		room2.add_connection(room1, REVERSE_DIRECTION[direction])
+
+func shuffleList(list):
+	var shuffledList = []
+	var indexList = range(list.size())
+	for i in range(list.size()):
+		randomize()
+		var x = randi()%indexList.size()
+		shuffledList.append(list[x])
+		indexList.remove(x)
+		list.remove(x)
+	return shuffledList
+
+
+
+
+func next_room(direction):
+	fade_screen()
+	current_room.visibility = false
+	var next_room = current_room.connections[direction]
+	current_room = next_room
+	move_player()
+	current_room.connections[direction].visibility = true
+	fade_screen()
+	
+func move_player():
+	pass
+func fade_screen():
+	pass
+
+func _ready() -> void:
+	make_level()
+	
+
--- src/Rooms/Room.gd
+++ src/Rooms/Room.gd
@@ -3,36 +3,83 @@ extends Node2D
 const SPAWN_EXPLOSION_SCENE: PackedScene = preload("res://src/Characters/SpawnExplosion.tscn")
 const ENEMY_SCENES:Dictionary={
 	"FLYING_CREATURE":preload("res://src/Characters/FlyingCreature.tscn")}
+
 	
 var num_enemies:int
-onready var tilemap:TileMap = get_node("Navigation2D/TileMap2")
+onready var tilemap:TileMap = get_node("TileMap2")
 onready var entrance: Node2D = get_node("Entrance")
 onready var door_container:Node2D = get_node("Doors")
 onready var enemy_positions_container:Node2D= get_node("EnemyPositions")
 onready var player_detector:Node2D = get_node("PlayerDetector")
 onready var center:Position2D = get_node("Center")
-	
+
+onready var connections = {"bot":null,
+						"top":null,
+						"right":null,
+						"left":null}
+
+###############################################################################################################################
+"""func _init():
+	var connections = {"bot":null,
+						"top":null,
+						"right":null,
+						"left":null}
+"""
+						
+func add_connection(room,direction):
+		if self.connections[direction] !=null: #Cannot override connections already made
+			return
+		self.connections[direction] = room
+
+func get_connection(direction):
+		return self.connections[direction]
+
+func available_directions():
+		var result:Array = []
+		for key in connections:
+			if self.connections[key] == null:
+				result.append(key)
+		return result
+		
+func num_connections():
+		var num:int = 0
+		for item in self.connections.values():
+			if item != null:
+				num+=1
+		return num
+
+#####################################################################################################################################	
+
+var is_cleared = false
 
 func _ready() -> void:
 	num_enemies= enemy_positions_container.get_child_count()
-	
+
+func is_room_cleared() ->bool:
+	return is_cleared
+
 func _open_doors() ->void:
-	for door in door_container.get_children():
-		door.open()
-	for entry_position in entrance.get_children():
-		#tilemap.set_cellv(tilemap.world_to_map(entry_position.position),999)
-		#tilemap.set_cellv(tilemap.world_to_map(entry_position.position)+ Vector2.DOWN,999)
-		_get_entrance(entry_position,false)
-		
+	if visible:
+		for door in door_container.get_children():
+			door.open()
+		for entry_position in entrance.get_children():
+			for point in entry_position.get_children():
+			#tilemap.set_cellv(tilemap.world_to_map(entry_position.position),999)
+			#tilemap.set_cellv(tilemap.world_to_map(entry_position.position)+ Vector2.DOWN,999)
+				_get_entrance(point,false)
+			
 		
 func _close_entrance() -> void:
-	for entry_position in entrance.get_children():
-		#tilemap.set_cellv(tilemap.world_to_map(entry_position.position),0)
-		#tilemap.set_cellv(tilemap.world_to_map(entry_position.position)+ Vector2.DOWN,1)
-		_get_entrance(entry_position,true)
+	if visible:
+		for entry_position in entrance.get_children():
+			for point in entry_position.get_children():
+			#tilemap.set_cellv(tilemap.world_to_map(entry_position.position),0)
+			#tilemap.set_cellv(tilemap.world_to_map(entry_position.position)+ Vector2.DOWN,1)
+				_get_entrance(point,true)
 		
 		
 func _spawn_enemies()-> void:
+	if visible:
 		for enemy_position in enemy_positions_container.get_children():
 			var enemy:KinematicBody2D = ENEMY_SCENES.FLYING_CREATURE.instance()
 			var __	= enemy.connect("tree_exited",self,"_on_enemy_killed")	
@@ -44,23 +91,27 @@ func _spawn_enemies()-> void:
 			call_deferred("add_child", spawn_explosion)
 			
 func _on_enemy_killed()->void:
-	num_enemies-=1
-	if num_enemies == 0:
-		_open_doors()
+	if visible:
+		num_enemies-=1
+		if num_enemies == 0:
+			is_cleared =true
+			_open_doors()
 
 
 func _on_PlayerDetector_body_entered(body: KinematicBody2D) -> void:
-	player_detector.queue_free()
-	_close_entrance()
-	_spawn_enemies()
+	if visible:
+		if body is KinematicBody2D:
+			player_detector.queue_free()
+			_close_entrance()
+			_spawn_enemies()
 
 func _get_entrance(entry_position,close):
-	if center.position.x < entry_position.position.x and abs(center.position.y - entry_position.position.y) <=30:
+	if center.position.x < entry_position.position.x and abs(center.position.y - entry_position.position.y) <=20:
 		if close:
 			_close_right(entry_position)
 		else:
 			_open_right(entry_position)
-	elif center.position.x > entry_position.position.x and abs(center.position.y - entry_position.position.y) <=30:
+	elif center.position.x > entry_position.position.x and abs(center.position.y - entry_position.position.y) <=20:
 		if close:
 			_close_left(entry_position)
 		else:
@@ -70,14 +121,16 @@ func _get_entrance(entry_position,close):
 			_close_bottom(entry_position)
 		else:
 			_open_bottom(entry_position)
-			
 
+
+
+########################################################################################################################################
 func _open_bottom(entry_position):
 	tilemap.set_cellv(tilemap.world_to_map(entry_position.position),999)
 	tilemap.set_cellv(tilemap.world_to_map(entry_position.position)+ Vector2.DOWN,999)
 func _close_bottom(entry_position):
-	tilemap.set_cellv(tilemap.world_to_map(entry_position.position),0)
-	tilemap.set_cellv(tilemap.world_to_map(entry_position.position)+ Vector2.DOWN,1)
+	tilemap.set_cellv(tilemap.world_to_map(entry_position.position),1)
+	tilemap.set_cellv(tilemap.world_to_map(entry_position.position)+ Vector2.DOWN,0)
 	
 func _open_right(entry_position) -> void:
 	tilemap.set_cellv(tilemap.world_to_map(entry_position.position),999)
@@ -89,4 +142,11 @@ func _open_left(entry_position) -> void:
 func _close_left(entry_position) -> void:
 	tilemap.set_cellv(tilemap.world_to_map(entry_position.position),3)
 
+
+
+#######################################################################################################################################
 	
+
+
+func _on_LeaveDetctor_area_entered(area: Area2D) -> void:
+	pass # Replace with function body.
